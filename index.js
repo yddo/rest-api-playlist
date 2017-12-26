5acab92a50dd9264bb61b7c0bc0d44435883b8a3
yddo <loker.yddo@gmail.com>
2017-12-26 20:07:18 +0200

    REST-API-Playlist tutorial

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+// set up express app
+const app = express();
+
+app.get('/api', function(req, res){
+  res.send({ name: 'Yddo' });
+});
+
+// listen for requests
+app.listen(process.env.port || 4000, function(){
+    console.log('now listening for requests');
+});
