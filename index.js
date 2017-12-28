49ded88dba960a61dd21556485b180f9e7f2db0b
yddo <loker.yddo@gmail.com>
2017-12-28 19:31:39 +0200

    added ninja models

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // set up express app
 const app = express();
 
-app.get('/api', function(req, res){
-  res.send({ name: 'Yddo' });
+// connect to mongodb
+mongoose.connect('mongodb://localhost/ninjago');
+mongoose.Promise = global.Promise;
+
+// use body-parser middleware
+app.use(bodyParser.json());
+
+// initialize routes
+app.use('/api', require('./routes/api'));
+
+// error handling middleware
+app.use(function(err, req, res, next){
+    console.log(err); // to see properties of message in our console
+    res.status(422).send({error: err.message});
 });
 
 // listen for requests
