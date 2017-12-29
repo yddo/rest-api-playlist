8938844cdc662e7584f289376275e99752248fdd
yddo <loker.yddo@gmail.com>
2017-12-29 13:18:41 +0200

    added html and css files

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const app = express();
 mongoose.connect('mongodb://localhost/ninjago');
 mongoose.Promise = global.Promise;
 
+app.use(express.static('public'));
+
 // use body-parser middleware
 app.use(bodyParser.json());
 
